a167690e4f9be9e8a8d664b0fc5abff742fa2a00
Omeke Gabriel <gabbyomekz92@gmail.com>
2022-06-30 15:40:55 -0700

    Print to standard output on C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Function description
+ * printf - To print to standard output
+ * Return: Brings the function to an end
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
