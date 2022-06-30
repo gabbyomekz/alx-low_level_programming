fba7b9b74a96a974fe08f632185b8aba4be5a26c
Omeke Gabriel <gabbyomekz92@gmail.com>
2022-06-30 15:25:42 -0700

    Puts.c commit on C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - This is a description about betty styles
+ * puts - Contains strings to be put out
+ * Return: Description where zero is success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
