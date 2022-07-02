e5cd04bcac35b04cad981e7d7679498685e5dd40
Omeke Gabriel <gabbyomekz92@gmail.com>
2022-07-01 21:14:01 -0700

    Smile in the mirror commit on C

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Declares function
+ * body - Follow instructions to do task
+ * printf or putchar - To print
+ * Return: 0 exits successfully
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+	putchar(ch);
+	}
+putchar(10);
+return (0);
+}
