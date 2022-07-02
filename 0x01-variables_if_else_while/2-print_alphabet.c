6ff6289ad1cb4b042851489d8b88ce755e72a3ca
Omeke Gabriel <gabbyomekz92@gmail.com>
2022-07-01 18:17:18 -0700

    Task 3 commit on putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Describes the function declaration
+ * print the last digit of the number stored in the variable
+ * Return: gives zero when successful
+*/
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+
+	return (0);
+}
