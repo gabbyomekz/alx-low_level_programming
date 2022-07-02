8708dd2597efc0dd8a54c331da52449552015660
Omeke Gabriel <gabbyomekz92@gmail.com>
2022-07-01 17:09:25 -0700

    If statements on C commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-	printf("if the number is greater than 0: is positive\n");
+	printf("%d is positive\n", n);
 }
 if (n == 0)
 {
-	printf("if the number is 0: is zero\n");
+	printf("%d is zero\n", n);
 }
 if (n < 0)
 {
-	printf("if the number is less than 0: is negative\n");
+	printf("%d is negative\n", n);
 }
 return (0);
 }
