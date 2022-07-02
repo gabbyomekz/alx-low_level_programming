a4541cae7e9085313f0737b29895b0e78a3e57eb
Omeke Gabriel <gabbyomekz92@gmail.com>
2022-07-01 20:38:24 -0700

    Numberz commit on C

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Declares the function
+ * body - Instructs on the task to be done
+ * printf or putchar - To display
+ * Return: Succes is 0
+*/
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+	putchar(ch);
+	}
+	putchar(10);
+	return (0);
+}	
