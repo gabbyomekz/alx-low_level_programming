d58c547a5dfb4c46ab29d2936ec3172900d047b7
Omeke Gabriel <gabbyomekz92@gmail.com>
2022-07-01 19:36:39 -0700

    Print numbers task 5 commit on C

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Describes the function
+ * body -program that prints all single digit numbers of base 10 startin from 0
+ * Return: zero for success
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d\n", n);
+	}
+return (0);
+}
