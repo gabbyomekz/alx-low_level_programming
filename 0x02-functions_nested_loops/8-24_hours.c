afc5a2ca530058812b4d36a208e3c77741b2ac1f
Omeke Gabriel <gabbyomekz92@gmail.com>
2022-07-05 15:28:40 -0700

    Jack Bauer 24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,33 +8,20 @@ void jack_bauer(void)
 {
 int i, j;
 
-for  (j = 0; j < 24; j++)
+i = 0;
+while (i < 24)
 {
-for (i = 0; i < 60; i++)
-{
-if (i < 10)
-{
-_putchar('0');
-_putchar(i + '0');
-}
-else if (i >= 10)
+j = 0;
+while (j < 60)
 {
 _putchar((i / 10) + '0');
 _putchar((i % 10) + '0');
-}
-if (j < 10)
-{
-_putchar(':');
-_putchar('0');
-_putchar(j + '0');
-}
-else if (j >= 10)
-{
 _putchar(':');
 _putchar((j / 10) + '0');
 _putchar((j % 10) + '0');
-}
 _putchar('\n');
+j++
 }
+i++
 }
 }
