526dafb011c5b4861eca966dd8a3ce2e121a08d2
Omeke Gabriel <gabbyomekz92@gmail.com>
2022-07-07 16:26:03 -0700

    printMostNumbers, from 0 to 9 but dont print 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function declared to print numbers from 0 to 9
+ * followed by a new line
+ * dont print 2 and 4
+ * void - doesn't require a return value
+ * Return: exits the function
+ */
+
+void print_most_numbers(void)
+{
+char m;
+
+for (m = 0; m < 10; m++)
+{
+if (m == 2 && m == 4)
+continue;
+_putchar('0' + m);
+}
+_putchar('\n');
+}
