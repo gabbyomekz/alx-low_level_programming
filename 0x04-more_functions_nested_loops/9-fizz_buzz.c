d9247a3e49b575310efa3363e8eef9d16df06fd6
Omeke Gabriel <gabbyomekz92@gmail.com>
2022-07-07 18:47:19 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - a function that prints the number 1 to 100
+ * prints Fizz for multiples of three
+ * prints Buzz for multiples of five
+ * prints FizzBuzz for multiples of three and five
+ * Each number or word should be seperated by a space
+ * Return: exits the program
+ */
+
+int main(void)
+{
+int n = 1;
+
+while (n <= 100)
+{
+if (n % 3 == 0 && n % 55 == 0)
+{
+printf("FizzBuzz");
+}
+else if (n % 3 == 0)
+{
+printf("Fizz");
+}
+else if (n % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", n);
+}
+if (n != 100)
+{
+printf(' ');
+}
+n++;
+}
+printf('\n');
+return (0);
+}
+
