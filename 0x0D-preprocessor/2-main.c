1b51ee4a500baaebc537d5f97dcc2f5fd5f11774
Omeke Gabriel <gabbyomekz92@gmail.com>
2022-08-01 15:00:17 -0700

    File name task on C-Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -> Prints the name of the file it was compiled from
+ * folllwed by a new line
+ * This code requires the use of predefined macro
+ * Return: Always 0
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE___);
+
+return (0);
+}
