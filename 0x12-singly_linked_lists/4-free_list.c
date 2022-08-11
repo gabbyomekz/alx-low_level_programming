19ea3c13dc95fb9b112c0a0afcff71fc4a2aa831
Omeke Gabriel <106773800+gabbyomekz@users.noreply.github.com>
2022-08-11 23:25:50 +0100

    Task 4 commit on Singly Linked Lists

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,21 +1,3 @@
-Skip to content
-UniqueEl
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-alx-low_level_programming/0x12-singly_linked_lists/4-free_list.c
-@UniqueEl
-UniqueEl singlylinked
- 1 contributor
-Executable File  19 lines (16 sloc)  266 Bytes
 #include "lists.h"
 
 /**
@@ -36,16 +18,3 @@ void free_list(list_t *head)
 	}
 }
 Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
