648eaa3220d3305ec383aec9d5c579313b521345
Omeke Gabriel <gabbyomekz92@gmail.com>
2022-10-03 15:36:33 -0700

    Functions nested loops copied to dynamic libraries

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - Describes a function to check for lowercase characters
+ * @c: Describes character argument
+ * Return: 0 or 1 depending on the outcome
+ */
+
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Describes a function that computes the absolute value of an integer
+ * @n: parameter
+ * Return: absolute value of a number
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+n = -n;
+return (n);
+}
